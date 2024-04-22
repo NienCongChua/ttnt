944ab4dcf8536de2776162e02cd1bac9cff332c4
NienCongChua <117887397+NienCongChua@users.noreply.github.com>
2024-04-22 14:57:26 +0700

    Add files via upload

---
+++ Bai8.2.py
@@ -0,0 +1,33 @@
+#Tim kiem theo chieu rong
+sodo = {
+    "A": ["C", "D", "E"],
+    "E": [],
+    "C": ["F"],
+    "F": ["M", "H"],
+    "M": [],
+    "H": [],
+    "D": ["I", "G"],
+    "I": [],
+    "G": ["R", "K"],
+    "R": [],
+    "K": []
+}
+
+
+def timkiem(mohinh, batdau, dich):
+    Mo = [batdau]
+    tmp = []
+    Dong = []
+    while Mo:
+        Dinh = Mo.pop(0)
+        if Dinh == dich:
+            print(dich, "-> La dich -> dung tim kiem")
+            break
+        if Dinh not in Dong:
+            Dong.append(Dinh)
+            print(Dinh, end=" ")
+            for Dinhke in mohinh[Dinh]:
+                Mo.append(Dinhke)
+
+
+timkiem(sodo, "A", "I")
